b69ca6f8ff34be27914d089e1e21ed0f31157d72
DennisNew1 <dennis.neumann@smail.th-koeln.de>
2020-10-20 18:31:14 +0200

    Add the dll functions to Unity Plugin

--- Unity Project/Assets/Plugins/FoveationPlugin.cs
+++ Unity Project/Assets/Plugins/FoveationPlugin.cs
@@ -14,13 +14,21 @@ public class FoveationPlugin : MonoBehaviour
     [DllImport(dll)]
     private static extern bool VrsSupported();
     [DllImport(dll)]
+    private static extern IntPtr GetRenderEventFunc();
+    [DllImport(dll)]
+    private static extern bool InitFoveation();
+    [DllImport(dll)]
+    private static extern bool SetGazeData(Vector3 leftEye, Vector3 rightEye);
+    [DllImport(dll)]
     private static extern int test();
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Ergebnis ist : " + test());
-        Debug.Log("Initialisierung der NvApi war erfolgreich: " + VrsSupported());
+        Debug.Log("VRS wird unterstützt: " + VrsSupported());
+
+        InitFoveation();
     }
 
     // Update is called once per frame
